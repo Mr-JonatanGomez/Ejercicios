b83e7219f16e94c3d0411f72eeaee6cbff0bd1f7
Mr-JonatanGomez <148049964+Mr-JonatanGomez@users.noreply.github.com>
2024-01-03 12:11:26 +0100

    añadido 5

--- Arrays/src/E5.java
+++ Arrays/src/E5.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 /*Realiza un programa que pida 8 números enteros,
 los guarde en un array y que luego muestre esos
 números junto con la palabra “par” o “impar” según proceda.*/
 public class E5 {
+    public static void main(String[] args) {
+        int[] numeros = new int[8];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("introduce 8 números");
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i]= sc.nextInt();
+        }
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i]%2==0){
+                System.out.println(numeros[i] + ": ES PAR");
+            }else {
+                System.out.println(numeros[i] + ": ES IMPAR");
+            }
+        }
+    }
 }

78825319bad3d75b82a2a6dadab3a6820b4428f5
Mr-JonatanGomez <148049964+Mr-JonatanGomez@users.noreply.github.com>
2024-01-03 12:25:59 +0100

    añadido 5

---
+++ Arrays/src/E6.java
@@ -0,0 +1,12 @@
+/*2. Escribe un programa que rellene un array de 20 números aleatorios entre el 0 y el 30. Una vez hecho esto realizará las siguientes operaciones:
+        - modificar todos los 6 por 8
+        - modificar todos los 7 por 15
+        - modificar todos los 20 por 10
+
+        Al finalizar el programa deberá indicar cuántas modificaciones se han realizado.
+        */
+public class E6 {
+    public static void main(String[] args) {
+
+    }
+}
